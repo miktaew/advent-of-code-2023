ea98fa1970a21c357f6ab679747b05be7fb84342
miktaew <miloszuszycki@gmail.com>
2023-12-02 18:25:34 +0100

    init; day1

---
+++ day1.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const day1 = ()=> {
+    const data = fs.readFileSync("day1.txt", "utf8").trim().split("\n");
+
+    const part2 = data.map(x => x.replaceAll("one", "o1e").replaceAll("two", "t2o").replaceAll("three", "t3e")
+                                 .replaceAll("four", "f4r").replaceAll("five", "f5e").replaceAll("six", "s6x")
+                                 .replaceAll("seven", "s7n").replaceAll("eight", "e8t").replaceAll("nine","n9e"));
+
+    //53531 too high
+
+    return {"part 1": to_num(data), "part 2": to_num(part2)};
+}
+
+const to_num = (arr) => {
+    return arr.map(x => x.replace(/[A-Za-z]/g, "")).map(x => Number.parseInt(x[0]+x[x.length-1])).reduce((a,b)=>a+b)
+}
+
+module.exports = day1;
