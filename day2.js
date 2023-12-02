7c2f3e62d7966ef28f063207fd44afea0a1a985a
miktaew <miloszuszycki@gmail.com>
2023-12-02 19:02:16 +0100

    shortened day2

--- day2.js
+++ day2.js
@@ -3,29 +3,21 @@ const day2 = ()=> {
     const data = fs.readFileSync("day2.txt", "utf8").trim().split("\n").map(x=>x.split(":")).map(x => x[1].split(/[;,]/).map(z => z.trim().split(" ")));
 
     const cubes = {red: 12, green: 13, blue: 14}
-
     let part1 = 0;
-    let impossible;
+    const powers = [];
+    
     for(let i = 0; i < data.length; i++) {
-        impossible = false;
+        let lowest = {red: 0, blue: 0, green: 0};
+        let impossible = false;
         for(let j = 0; j < data[i].length; j++) {
             if(Number.parseInt(data[i][j][0]) > cubes[data[i][j][1]]) {
                 impossible = true;
-                break;
             }
+
+            lowest[data[i][j][1]] = Math.max(lowest[data[i][j][1]], Number.parseInt(data[i][j][0]));
         }
         if(!impossible) {
             part1 += i+1;
-        }
-    }
-
-    const powers = [];
-    let lowest = {};
-    for(let i = 0; i < data.length; i++) {
-        lowest = {red: 0, blue: 0, green: 0}
-        for(let j = 0; j < data[i].length; j++) {
-            const num = Number.parseInt(data[i][j][0]);
-            lowest[data[i][j][1]] = Math.max(lowest[data[i][j][1]], num);
         }
         powers.push(lowest.red*lowest.blue*lowest.green);
     }
